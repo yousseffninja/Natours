203ee30259adaede5f2c683cc94aaf8050e69c80
Youssef Mohamed <81262678+yousseffninja@users.noreply.github.com>
2022-08-21 19:23:19 +0200

    Continue Project

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,29 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1) Filtering
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    // 2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => `$${match}`
+    );
+
+    console.log(JSON.parse(queryStr));
+    console.log(req.query, queryObj);
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
--- server.js
+++ server.js
@@ -1,4 +1,27 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: './config.env',
+});
 const app = require('./app');
+
+// console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    // .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
